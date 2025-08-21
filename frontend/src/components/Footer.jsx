e437603f58658520524d9d48fa74161243f8d112
Gemtessa Deksisa <gemtessadeksisa@gmail.com>
2025-08-22 01:07:45 +0900

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,67 +1,9 @@
-fjjjh
-f
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white text-center py-4">
+        <p>© 2025 INSA , Language Learning Platform</p>
+      </footer>
+    );
+  };
   
-
-  
+  export default Footer;
