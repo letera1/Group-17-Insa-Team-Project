c7a500afb705f2eb595133b85f1051160257f54f
Gemtessa Deksisa <gemtessadeksisa@gmail.com>
2025-08-24 22:34:07 +0900

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import useFriendRequestNotifications from "../hooks/useFriendRequestNotifications";
-
 const Layout = ({ children, showSidebar = false }) => {
   // Initialize friend request notifications
   useFriendRequestNotifications();
@@ -9,7 +8,6 @@ const Layout = ({ children, showSidebar = false }) => {
     <div className="min-h-screen">
       <div className="flex">
         {showSidebar && <Sidebar />}
-
         <div className="flex-1 flex flex-col">
           <Navbar />
           <main className="flex-1 overflow-y-auto">{children}</main>
