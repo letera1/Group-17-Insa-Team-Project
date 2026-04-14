f61be6a33aecc00bf808f9e2471e93e7c5080427
Letera Tujo <letera9tujo@gmail.com>
2026-04-14 16:33:34 +0300

    complete

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -1,24 +1,5 @@
 import { Link } from "react-router";
-import { LANGUAGE_TO_FLAG } from "../constants";
-
-// Helper function to get language flag
-function getLanguageFlag(language) {
-  if (!language) return null;
-
-  const langLower = language.toLowerCase();
-  const countryCode = LANGUAGE_TO_FLAG[langLower];
-
-  if (countryCode) {
-    return (
-      <img
-        src={`https://flagcdn.com/24x18/${countryCode}.png`}
-        alt={`${langLower} flag`}
-        className="h-3 mr-1 inline-block"
-      />
-    );
-  }
-  return null;
-}
+import { getLanguageFlag } from "../lib/languageUtils";
 
 const FriendCard = ({ friend }) => {
   return (
