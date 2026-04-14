d732ed0e0cbb664468d4d04b629f90ac34e98a5b
Letera Tujo <letera9tujo@gmail.com>
2026-04-14 16:34:54 +0300

    complete

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { getLanguageFlag } from "../lib/languageUtils";
+import { getLanguageFlag } from "../lib/languageUtils.jsx";
 
 const FriendCard = ({ friend }) => {
   return (
